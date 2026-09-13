cdf558b3df62c04cda19d48a1909c2def38b503c
Jorge Matos <jisapjisap@gmail.com>
2026-09-13 11:03:12 +0200

    feat: add BookingProcess section to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./sections/HeroSection"
 import Stats from "./sections/Stats"
 import Dishes from "./sections/Dishes"
 import Features from "./sections/Features"
+import BookingProcess from "./sections/BookingProcess"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Stats />
       <Dishes />
       <Features />
+      <BookingProcess />
     </>
   )
 }
